12f64eebb0180481cb6c7d967e77e466ed1b5a56
viswanathmv <viswanath.mv@gmail.com>
2025-01-11 20:47:09 +0530

    Third Commit

--- apm-begin/src/app/products/product-detail/product-detail.component.ts
+++ apm-begin/src/app/products/product-detail/product-detail.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
-import { NgIf, NgFor, CurrencyPipe ,AsyncPipe} from '@angular/common';
+
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 import { async, catchError, EMPTY, Observable } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { NgIf, NgFor, CurrencyPipe, AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
     selector: 'pm-product-detail',
     templateUrl: './product-detail.component.html',
     standalone: true,
-    imports: [NgIf, NgFor, CurrencyPipe,AsyncPipe]
+    imports: [NgIf, NgFor, CurrencyPipe,AsyncPipe,FormsModule,CommonModule]
 })
 export class ProductDetailComponent {
   // Just enough here for the template to compile
